7a6a276b9e1f648d3fd0be1dda0c6f2bbfccff63
JustFiesta <mateusz.bocak9@gmail.com>
2023-08-11 14:36:13 +0200

    Added comments

--- showCPUusg.sh
+++ showCPUusg.sh
@@ -1,2 +1,6 @@
-ps aux | awk 'END {print "Liczba uruchomionych procesów: " NR-1}'
-top -b -n 1 | awk '/^%CPU/ {cpu=$2} /^MiB Mem/ {ram=$8} END {print "CPU: " cpu " %", "RAM: " ram}'
+#Goal is to create script for cron jobs
+
+ps aux | awk 'END {print "Liczba uruchomionych procesów: " NR-1}'  # aux - show all processes (BSD syntax), trunkated to awk - with prints at end of ps number of processes (- first column)
+top -b -n 1 | awk '/^%CPU/ {cpu=$2} /^MiB Mem/ {ram=$8} END {print "CPU: " cpu " %", "RAM: " ram}' #top opened once, trunkated to awk - it finds line starting with %CPU and MiB Mem, and assigns it to variable, than prints it
+
+#after adding this script to machine add it's path to crontab -e
