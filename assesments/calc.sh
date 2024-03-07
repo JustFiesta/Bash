fea1e6455920071199735c40ba2cfc990d16e5a9
Matthew <99048876+JustFiesta@users.noreply.github.com>
2024-03-07 18:18:02 +0100

    calc scratch

--- assesments/calc.sh
+++ assesments/calc.sh
@@ -1,20 +1,74 @@
 #!/bin/bash
+# simple calculator using getopts
 
-#oflag=
-#nflag=
-#dflag=
-#
-# while getopts ond: name
-# do
-#   case $name in
-#   o)oflag=true
-#     oARG="$OPTARG"  
-#     ;;
-#   n)nflag=true;;
-#   d)dflag=true;;
-#   \?) handle error
-#   :) "$OPTARG requires an argument" >&2
-#   esac
-#done
-#
-#if [[ flaga cos tam to sprawdz jej argumenty ]] 
+# variables and config
+operation=""
+d_flag=0
+
+# script - start
+while getopts "o:n:d" option; do
+
+    case $option in
+        o)  
+            case $OPTARG in
+                +)
+                    operation="+"
+                    ;;
+                -) 
+                    operation="-"
+                    ;;
+                \*)
+                    operation="\*"
+                    ;;
+                \%)
+                    operation="\%"
+                    ;;
+                \?)
+                    echo "Invalid operation! Choose from these (+, -, *, %)"
+                    ;;
+            esac
+            ;;
+        n)
+            set -f # disable glob
+            IFS=" " # split by space
+            nums+=("$OPTARG")
+            while [[ ${!OPTIND} && ${!OPTIND} != -* ]]; do
+			    nums+=( "${!OPTIND}" )
+			    ((OPTIND++))
+            done
+            ;;
+        d)
+           d_flag=1
+            ;;
+        \?) 
+            echo "Invalid option: -$OPTARG"
+            ;;
+    esac
+done
+
+shift $(( OPTIND-1 ))
+
+echo "The whole list of values is '${nums[@]}'"
+
+echo "given nums: "
+for i in "${nums[@]}"; do
+    echo -n " ${i}"
+done 
+
+if [ ! $d_flag -eq 1 ]; then
+    case $operation in
+        +)
+            
+            ;;
+        -) 
+
+            ;;
+        \*)
+
+            ;;
+        \%)
+
+            ;;
+    esac
+
+fi    
