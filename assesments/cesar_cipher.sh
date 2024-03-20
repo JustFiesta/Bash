4fae96793e6f88ee3c1dc21d21c3099e8f72edd3
JustFiesta <mateusz.bocak9@gmail.com>
2024-03-20 09:51:36 +0100

    cesar cipher rebase - remove redundant cat

--- assesments/cesar_cipher.sh
+++ assesments/cesar_cipher.sh
@@ -20,12 +20,7 @@ encode () {
     echo "encoding for $1 $2 $3"
 
     #ROT-13
-    cat $2 | tr "[A-Za-z]" "[N-ZA-Mn-za-m]"  > $3
-    
-    #cat "$2" | tr "A-Za-z" "$(echo A-Za-z | tr "A-Za-z" "$(echo {A-Za-z} | sed "s/.*\n//")")"  > $3
-    #cat "$2" | tr "A-Za-z" "$(echo A-Za-z | tr "A-Za-z" "$(echo {A-Za-z} | cut -b "$1"-26)$(echo {A-Za-z} | cut -b 1-"$(($1-1))")" > $3
-    
-    #cat $2 | tr "[A-Za-z]" "$(printf %${shift}s | tr ' ' 'A-Za-z' | tr 'A-Za-z' 'A-Za-z')$(printf %${shift}s | tr ' ' '[a-z]' | tr 'a-z' 'a-z')"  > $3
+    tr "[A-Za-z]" "[N-ZA-Mn-za-m]" < $2 > $3
 }
 
 # function declaration - stop
