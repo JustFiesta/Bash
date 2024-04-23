ce9f337a3d6e523f58c756479af930ef2b63e8a2
JustFiesta <mateusz.bocak9@gmail.com>
2024-04-23 11:13:51 +0200

    fixup: input file validation - must be plan text, numver validation, exit statuses

--- assesments/cesar_cipher.sh
+++ assesments/cesar_cipher.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # text encryption in cesar cipher
+# supports only small letters up to ROT26 
 
 # ===========================
 # $ ./cesar_cipher.sh -s <shift> -i <input file> -o <output file>
@@ -18,7 +19,7 @@ alphabet=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrs
 # function declaration - start
 # function to encode characters in given file
 encode () {
-    tr "${alphabet:0:26}" "${alphabet:${shift}:26}"
+    tr "${alphabet:0:26}" "${alphabet:${shift}:26}" < $1 > $2
 }
 
 # function declaration - stop
@@ -48,13 +49,22 @@ while getopts ":s:i:o:h" opt; do
     esac
 done
 
-if [ "$input_file" == "" ] || [ "$output_file" == "" ]; then
-    echo "Please provide input and output filenames"
-elif [ ! -s "$input_file" ]; then
-    echo "Provide input file with text contents!"
-else
-    encode "$shift" "$input_file" "$output_file" 
 
+file_type=$(file -b --mime-type "$input_file")
+
+if [ "$input_file" == "" ] || [ "$output_file" == "" ]; then #check for filenames
+    echo "Please provide input and output filenames"
+    exit 1
+elif [[ $file_type != "text/plain" ]]; then # check for file type (must be text)
+    echo "Input file is not a text file!"
+    exit 1
+elif [ $shift -gt 26 ] || [ $shift -lt 0 ]; then # check for positive num in (0-26 range)
+    echo "Provide valid shift! (0-26)"
+    exit 1
+else
+    encode "$input_file" "$output_file" 
 fi
+
+exit 0
 
 # script - stop 
