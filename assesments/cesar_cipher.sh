3d2e26a9afb081fea7e62579515fba0b47e0ba09
JustFiesta <mateusz.bocak9@gmail.com>
2024-04-24 12:14:55 +0200

    Added possability for space in filename

--- assesments/cesar_cipher.sh
+++ assesments/cesar_cipher.sh
@@ -20,7 +20,7 @@ uppercase_alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"
 # function declaration - start
 # function to encode characters in given file
 encode () {
-    tr "${lowercase_alphabet:0:26}" "${lowercase_alphabet:${shift}:26}" < $1 | tr "${uppercase_alphabet:0:26}" "${uppercase_alphabet:${shift}:26}" > $2
+    tr "${lowercase_alphabet:0:26}" "${lowercase_alphabet:${shift}:26}" < "$1" | tr "${uppercase_alphabet:0:26}" "${uppercase_alphabet:${shift}:26}" > $2
 }
 
 # function declaration - stop
@@ -34,9 +34,19 @@ while getopts ":s:i:o:h" opt; do
             ;;
         i)
             input_file="$OPTARG"
+            # get all option arguments and treat it like single string/filename
+            while [[ ${!OPTIND} && ${!OPTIND} != -* ]]; do
+                input_file+=" ${!OPTIND}"
+                ((OPTIND++))
+            done
             ;;
         o)
             output_file="$OPTARG"
+            # get all option arguments and treat it like single string/filename
+            while [[ ${!OPTIND} && ${!OPTIND} != -* ]]; do
+                output_file+=" ${!OPTIND}"
+                ((OPTIND++))
+            done
             ;;
         h)
             echo "Usage: -s <shift> -i <input file> -o <output file>"
@@ -50,21 +60,31 @@ while getopts ":s:i:o:h" opt; do
     esac
 done
 
+#check for correct filenames
+if [ "$input_file" == "" ]; then
+    echo "Please provide input filename"
+    exit 1
+fi
 
+if [ "$output_file" == "" ]; then
+    echo "Please provide output filename"
+    exit 1
+fi
+
+# get filetype
 file_type=$(file -b --mime-type "$input_file")
 
-if [ "$input_file" == "" ] || [ "$output_file" == "" ]; then #check for filenames
-    echo "Please provide input and output filenames"
-    exit 1
-elif [[ $file_type != "text/plain" ]]; then # check for file type (must be text)
+if [[ $file_type != "text/plain" ]]; then # check for file type (must be text)
     echo "Input file is not a text file!"
     exit 1
-elif [ $shift -gt 26 ] || [ $shift -lt 0 ]; then # check for positive num in (0-26 range)
-    echo "Provide valid shift! (0-26)"
-    exit 1
-else
-    encode "$input_file" "$output_file" 
 fi
+
+if [ $shift -gt 26 ] || [ $shift -lt 0 ]; then # check for positive num in (0-26 range)
+    echo "Provide valid shift! (0-26)"
+    exit 1 
+fi
+
+encode "$input_file" "$output_file" 
 
 exit 0
 
